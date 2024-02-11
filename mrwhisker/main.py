4fe8513ea40cb56b074ae97380a86df93223379b
josiah <josiah@appstack.nyc>
2024-02-11 14:31:31 -0500

    error if template is not a string or a filestream

--- mrwhisker/main.py
+++ mrwhisker/main.py
@@ -3,6 +3,12 @@ from io import TextIOWrapper
 
 
 def render(template: Union[str, TextIOWrapper]) -> str:
+    """
+    Render a mustache template
+    """
+    if not isinstance(template, (str, TextIOWrapper)):
+        raise TypeError("Template must be a string or file object")
+
     if isinstance(template, TextIOWrapper):
         template = template.read()
     return template
--- tests/test_render.py
+++ tests/test_render.py
@@ -1,10 +1,17 @@
+import pytest
 from mrwhisker.main import render
 
 
 def test_render_string():
     assert render("I'm Mr. Whisker") == "I'm Mr. Whisker"
 
+
 def test_render_html():
     with open("tests/test.html") as html:
-      assert render(html) == "<h1>Mr. Whisker</h1>"
+        assert render(html) == "<h1>Mr. Whisker</h1>"
 
+
+def test_raise_error_template_not_string_or_file():
+    with pytest.raises(TypeError) as e:
+        render(123)
+    assert str(e.value) == "Template must be a string or file object"
