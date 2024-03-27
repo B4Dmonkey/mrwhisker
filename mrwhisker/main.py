432676369633aba6264f266845ef4064e967b923
josiah <josiah@appstack.nyc>
2024-03-27 07:10:41 -0400

    handle basic interpolation

--- mrwhisker/main.py
+++ mrwhisker/main.py
@@ -1,8 +1,10 @@
 import re
 import html
 from enum import Enum
-from typing import Union, List, Tuple, Generator
+from typing import Union, Generator
 from io import TextIOWrapper
+
+RAW_HTML_TOKEN_MATCHER = r'{{{\s*.*\s*}}}|{{&\s*.*\s*}}'
 
 
 def render(template: Union[str, TextIOWrapper], data: Union[str, dict] = '') -> str:
@@ -26,9 +28,6 @@ def render(template: Union[str, TextIOWrapper], data: Union[str, dict] = '') -> 
     if isinstance(template, TextIOWrapper):
         template = template.read()
 
-    if isinstance(data, str):
-        return re.sub(r'{{\s*(\w+)\s*}}', data, template)
-
     # * Find all the tokens in the template
     tokens = tokenize(template, data)
 
@@ -36,95 +35,19 @@ def render(template: Union[str, TextIOWrapper], data: Union[str, dict] = '') -> 
         template = token.eval(template)
 
     return template
-    # tokens, template = match_tokens(template, data)
-
-    # if data and isinstance(data, dict):
-    #     for token in tokens:
-    #         # * If the token is not in the template, remove it from the template
-    #         if not bool(re.search(token.key, template)):
-    #             template = re.sub(token.key, '', template)
-    #             continue
-
-    #         value = token.value
-
-    #         if token.type == TokenType.VARIABLE:
-    #             value = html.escape(value, quote=False)
-
-    #         # ! need to fix the matching here
-    #         template = template.replace(token.key, value)
-
-    # return template
 
 
 # TOKEN_MATCHER = r'({{2,3}\s*.*?\s*}{2,3})'
 TOKEN_MATCHER = r'({{2,3}\s*\S*?\s*}{2,3})'
 
 
-# def match_tokens(template: str, data: Union[str, dict]) -> Tuple[List, str]:
-#     tokens = []
-
-#     def remove_spaces(match):
-#         return match.group(1) + match.group(2).strip() + match.group(3)
-
-#     for match in re.finditer(TOKEN_MATCHER, template):
-#         token_match = match.group()
-#         template = re.sub(r'({{2,3}&?)\s*(.*?)\s*(}{2,3})',
-#                           remove_spaces, template)
-
-#         token_key = re.sub(r'\s+', '', token_match)
-#         # ! This feels wrong
-#         unwanted_chars = "{ s* & } \\"
-#         token_name = ''.join(c for c in token_key if c not in unwanted_chars)
-
-#         if data and isinstance(data, dict):
-#             value = data.get(token_name, '')
-
-#         if not data or isinstance(data, str):
-#             value = data
-#         tokens.append(
-#             Token(key=token_key, value=value)
-#         )
-
-#     return tokens, template
-
-
 VARIABLE_TOKEN_MATCHER = r'{{\s*(\w+)\s*}}'
 # RAW_HTML_TOKEN_MATCHER = r'{{{\s*(\w*)\s*}}}|{{&\s*(\w*)\s*}}'
-RAW_HTML_TOKEN_MATCHER = r'{{{\s*.*\s*}}}|{{&\s*.*\s*}}'
 
 
 class TokenType(Enum):
     VARIABLE = 'variable'
     RAW_HTML = 'raw_html'
-
-
-# class Token:
-#     def __init__(self, key: str, value: str = None):
-#         self._key = key
-#         self._value = value
-
-#     def __eq__(self, other):
-#         if isinstance(other, Token):
-#             return self.key == other.key
-#         return False
-
-#     @property
-#     def key(self):
-#         return self._key
-
-#     @property
-#     def value(self):
-#         return self._value if self._value else ''
-
-#     @property
-#     def type(self):
-#         match self.key:
-#             case str() if re.match(r'{{{\s*.*\s*}}}|{{&\s*.*\s*}}', self.key):
-#                 return TokenType.RAW_HTML
-#             case  str() if re.match(r'{{\s*.*\s*}}', self.key):
-#                 return TokenType.VARIABLE
-#             case _:
-#                 raise ValueError("Invalid token type")
 
 
 class Token:
@@ -146,15 +69,49 @@ def has_triple_mustache(value: str) -> bool:
     return '{{{' in value and '}}}' in value
 
 
-def tokenize(template: str, data: dict) -> Generator[Token, None, None]:
+def has_ampersand(value: str) -> bool:
+    """
+    Checks if a value has an ampersand
+    Args:
+    value (str): The value to check
+    return (bool): True if the value has an ampersand, False otherwise
+    """
+    return '&' in value
+
+
+def is_html_escape(value: str) -> bool:
+    """
+    Checks if a value is html escaped
+    Args:
+    value (str): The value to check
+    return (bool): True if the value is html escaped, False otherwise
+    """
+    if has_triple_mustache(value):
+        return False
+    if has_ampersand(value):
+        return False
+    return True
+
+
+def tokenize(template: str, data: Union[str, dict]) -> Generator[Token, None, None]:
     tokens = []
     for match in re.finditer(TOKEN_MATCHER, template):
         token_match = match.group()
-        token_var = token_match.strip('{}')
-        token_value = data.get(token_var, '')
+        token_var = token_match.strip('{&}')
+        token_value = data if isinstance(
+            data, str) else data.get(token_var, '')
+
+        if not token_value:
+            token_value = ''
+
+        # * convert int to string
+        if isinstance(token_value, (int, float)):
+            token_value = str(token_value)
+
         # * escape html characters
-        if not has_triple_mustache(token_match):
+        if is_html_escape(token_match):
             token_value = html.escape(token_value)
+
         tokens.append(
             Token(key=token_match, value=token_value)
         )
